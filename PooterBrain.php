aeb4791e18e530fd8b989fe9dc1ea261101c7da0
danieleambrosino <danieleambrosino97@gmail.com>
2017-05-19 00:44:27 +0200

    Minor fixes

--- PooterBrain.php
+++ PooterBrain.php
@@ -32,19 +32,22 @@ class PooterBrain
 
   }
 
-  public function interpret() {
+  public function interpret()
+  {
     $text_to_send = "";
 
-    if (strpos($text, 'ciao') !== FALSE || strpos($text, '/start') !== FALSE) {
+    if (strpos($this->text, 'ciao') !== FALSE || strpos($this->text, '/start') !== FALSE) {
       $text_to_send = 'Ciao ' . $this->interlocutor_name . ', caro amico mio, io sono Pietro Gusso. Ho 20 anni e mi piace la musica e lo sport e da ben 9 anni pratico rugby!';
-    } elseif (strpos($text, 'zitto') !== FALSE) {
+    } elseif (strpos($this->text, 'zitto') !== FALSE) {
       $text_to_send = $this->interlocutor_name . ' potresti rispettare le persone che scrivono quello che vogliono? Senza offesa per te, ma potresti non cagare il cazzo?';
-    } elseif (strpos($text, 'somebody') !== FALSE) {
+    } elseif (strpos($this->text, 'somebody') !== FALSE) {
       $text_to_send = 'Eh grande pezzo';
-    } elseif (strpos($text, 'fidanzata') !== FALSE || strpos($text, 'sono') !== FALSE ||
-      strpos($text, 'sharade') !== FALSE || strpos($text, 'charade') !== FALSE)
-    {
+    } elseif (strpos($this->text, 'fidanzata') !== FALSE || strpos($this->text, 'sono') !== FALSE ||
+      strpos($this->text, 'sharade') !== FALSE || strpos($this->text, 'charade') !== FALSE
+    ) {
       $text_to_send = 'Sono Speedy Gonzales?';
+    } elseif (strpos($this->text, 'stopper') !== FALSE) {
+      $text_to_send = 'Sergio Brio!';
     }
 
     return $text_to_send;
